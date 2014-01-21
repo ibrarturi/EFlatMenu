79044840ff841e67129052ba6c79cd8577de70bf
Ibrar Hussain <ibrarturi@gmail.com>
2014-01-21 16:51:43 +0500

    removing extra code

--- views/view.php
+++ views/view.php
@@ -55,28 +55,5 @@
             echo CHtml::closeTag('li');
         }
         ?>
-
-<!--        <li><a href="#"><i class="icon-home"></i>HOME</a>
-            <ul class="sub-menu">
-                <li><a href="#">Sub-Menu 1</a></li>
-                <li><a href="#">Sub-Menu 2</a></li>
-                <li><a href="#">Sub-Menu 3</a></li>
-            </ul>
-        </li>
-        <li><a  href="#"><i class="icon-user"></i>ABOUT</a></li>
-        <li><a  href="#"><i class="icon-camera"></i>PORTFOLIO</a>
-            <ul class="sub-menu">
-                <li><a href="#">Sub-Menu 1</a></li>
-                <li><a href="#">Level 3 Menu</a>
-                    <ul>
-                        <li><a href="#">Sub-Menu 4</a></li>
-                        <li><a href="#">Sub-Menu 5</a></li>
-                        <li><a href="#">Sub-Menu 6</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </li>
-        <li><a  href="#"><i class="icon-bullhorn"></i>BLOG</a></li>
-        <li><a  href="#"><i class="icon-envelope-alt"></i>CONTACT</a></li>-->
     </ul>
 </nav>
