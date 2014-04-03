1126e9a385d8b49ead8c818e7f11c19be0a16b1d
Ibrar Hussain <ibrarturi@gmail.com>
2014-04-03 09:58:42 +0500

    adjusting font-awesome css file path

--- EFlatMenu.php
+++ EFlatMenu.php
@@ -18,17 +18,8 @@ class EFlatMenu extends CWidget {
         $scriptUrl = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('application.extensions.eflatmenu.resources'));
  
         $cs->registerCssFile($scriptUrl . '/eflatmenu.css');
-        $cs->registerCssFile($scriptUrl . '/font-awesome/font-awesome.css');
+        $cs->registerCssFile($scriptUrl . '/font-awesome/css/font-awesome.css');
         $cs->registerScriptFile($scriptUrl . '/eflatmenu.js');
-    }
-
-    protected function registerClientScript() {
-        $cs = Yii::app()->clientScript;
-
-        $cs->registerCssFile($this->cssFile);
-        $cs->registerCssFile($this->fontawesomeCSSFile);
-        $cs->registerCoreScript('jquery');
-        $cs->registerScriptFile($this->jsFile, CClientScript::POS_END);
     }
 
     public function run() {
